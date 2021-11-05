f2d9fb4e8669e2849068260593c4e0fbd6478663
qqqestion <toronto.city.nick@gmail.com>
2021-11-05 17:02:35 +0300

    feat: add SetInfoFragment

---
+++ app/src/main/java/ru/tashkent/messenger/ui/setinfo/SetInfoFragment.kt
@@ -0,0 +1,18 @@
+package ru.tashkent.messenger.ui.setinfo
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import ru.tashkent.messenger.R
+import ru.tashkent.messenger.databinding.FragmentSetInfoBinding
+import ru.tashkent.messenger.viewbinding.viewBinding
+
+class SetInfoFragment : Fragment(R.layout.fragment_set_info) {
+
+    private val binding: FragmentSetInfoBinding by viewBinding()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
