9b2486462968f1e442adff58ab4442283e7e4999
qqqestion <toronto.city.nick@gmail.com>
2021-11-05 16:49:51 +0300

    feat: add extension for task which transform task to either

--- data/src/main/java/ru/tashkent/data/exts/TaskExt.kt
+++ data/src/main/java/ru/tashkent/data/exts/TaskExt.kt
@@ -2,6 +2,7 @@ package ru.tashkent.data
 
 import com.google.android.gms.tasks.Task
 import kotlinx.coroutines.tasks.await
+import ru.tashkent.domain.Either
 
 
 suspend fun <T> Task<T>.awaitResult(): Result<T> {
@@ -9,5 +10,13 @@ suspend fun <T> Task<T>.awaitResult(): Result<T> {
         Result.success(await())
     } catch (e: Exception) {
         Result.failure(e)
+    }
+}
+
+suspend fun <T> Task<T>.awaitTask(): Either<Throwable, T> {
+    return try {
+        Either.Right(await())
+    } catch (e: Exception) {
+        Either.Left(e)
     }
 }
