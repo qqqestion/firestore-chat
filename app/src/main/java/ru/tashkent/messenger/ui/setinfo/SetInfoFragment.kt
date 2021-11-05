1aa458e347d6bff473d815a4a7942b52f6526a78
qqqestion <toronto.city.nick@gmail.com>
2021-11-05 17:24:03 +0300

    feat: finish layout for SetInfoFragment.kt

--- app/src/main/java/ru/tashkent/messenger/ui/setinfo/SetInfoFragment.kt
+++ app/src/main/java/ru/tashkent/messenger/ui/setinfo/SetInfoFragment.kt
@@ -3,6 +3,7 @@ package ru.tashkent.messenger.ui.setinfo
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import ru.tashkent.messenger.R
 import ru.tashkent.messenger.databinding.FragmentSetInfoBinding
 import ru.tashkent.messenger.viewbinding.viewBinding
@@ -13,6 +14,8 @@ class SetInfoFragment : Fragment(R.layout.fragment_set_info) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        binding.tvSubmit.setOnClickListener {
+            findNavController().navigate(SetInfoFragmentDirections.actionSetInfoFragmentToMyChatsFragment())
+        }
     }
 }
