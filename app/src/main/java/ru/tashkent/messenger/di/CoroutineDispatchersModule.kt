9550a66130fb71edb31bd53157028c2009327a43
qqqestion <toronto.city.nick@gmail.com>
2021-11-19 20:30:09 +0300

    fix: make CoroutineDispatchersModule.kt an actual module

--- app/src/main/java/ru/tashkent/messenger/di/CoroutineDispatchersModule.kt
+++ app/src/main/java/ru/tashkent/messenger/di/CoroutineDispatchersModule.kt
@@ -1,9 +1,11 @@
 package ru.tashkent.messenger.di
 
+import dagger.Module
 import dagger.Provides
 import kotlinx.coroutines.Dispatchers
 import ru.tashkent.domain.CoroutineDispatchers
 
+@Module
 class CoroutineDispatchersModule {
 
     @Provides
